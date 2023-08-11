9651491e4d2486a9c39a33f31a15c74e36606af1
Denzel Gitonga <gitongadenzel@gmail.com>
2023-08-11 15:19:14 +0300

    model

--- sales/models.py
+++ sales/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 class Sale(models.Model):
     sold_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, editable=False)
-    date = models.DateTimeField(auto_now_add=True, max_length=6)
+    date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     rendered_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     balance = models.DecimalField(default=0, max_digits=10, decimal_places=2)
